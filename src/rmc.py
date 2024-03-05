f387d6c3591c0caf8676e4d1cf3d418e75f89583
Valeria Deloya <anavaleriadeloya@ciencias.unam.mx>
2024-03-05 16:54:50 -0600

    restricciones

--- src/rmc.py
+++ src/rmc.py
@@ -21,18 +21,38 @@ k = archivo.readline()
 #Almacenamos las aristas en una lista
 aristas = []
 #Para almacenar el numero de aristas
-num = 0
+num_ar = 0
 
 #Leyendo el archivo a partir de la linea 3
 for linea in archivo:
     aristas.append(linea.strip('\n'))
-    num += 1 #y va eliminando los \n
+    num_ar += 1 #y va eliminando los \n
 
 
 def aristas_y_vertices():
 #ESPECIFICACIONES, revisamos que se cumpla las restricciones que pide la practica.
 
-    return 0
+    especificaciones = False
+
+    #Para almacenar la cantidad de vertices
+    #Como la practica pide el formato de los vertices,
+    #en la primer linea del .txt la cantidad de vertices es la cantidad de comas + 1
+    num_vr = 1
+
+    #Para eliminar espacios, simbolos *
+    for i in vertices:
+
+        if i == ',':
+            num_vr += 1
+
+    #Para las especificaciones para los vertices y aristas
+    if 10 <= num_vr and num_vr <= 20 and num_ar > (2 * num_vr):
+        especificaciones = True
+    else:
+        especificaciones = False
+
+    return especificaciones
+
 
 def trayectoria():
 #FASE ADIVINADORA.
@@ -40,17 +60,31 @@ def trayectoria():
 #a arrojar un valor aleatorio entre 0 y 1 es que vamos a decidir si incluir o no a la arista en la trayectoria.
 #Si es 1 la incluye en la trayectoria y si es 0 no.
 
-    nd_choice = random.randint(0,1)
     trayect = []
 
-    for i in range(len(aristas)):
-        
+    for i in aristas:
+
+        nd_choice = random.randint(0,1)
+
         if nd_choice == 1:
             trayect.append(i)
     
     return trayect
 
+
+def main(formula):
+#Funcion main para mandar a llamar todo, ordenado.
+
+    esp = aristas_y_vertices()
+
+    if esp == True:
+        print('wuu')
+
+    else: 
+        print('Grafica invalida')
+
 print(str(vertices))
 print(str(k))
 print(aristas)
-print(num)
+print(num_ar)
+print(trayectoria())
