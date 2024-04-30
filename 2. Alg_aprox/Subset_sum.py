87d732b732ef3a86aee3406ad3c44584dc00061c
Valeria Deloya <anavaleriadeloya@ciencias.unam.mx>
2024-04-29 22:22:41 -0600

    merge lists

--- 2. Alg_aprox/Subset_sum.py
+++ 2. Alg_aprox/Subset_sum.py
@@ -52,10 +52,43 @@ def TRIM(conjunto, delta):
 
 
 def MERGE_LISTS(lista1, lista2):
-#Devuelve una lista ordenada, siento esta la fusión de sus dos listas de entrada ordenadas,
+#Devuelve una lista ordenada, siendo esta la fusión de sus dos listas de entrada ordenadas,
 # con valores duplicados eliminados.
 
-    return True
+#El pseudocodigo de esta funcion fue omitido en el libro.
+
+    l1 = len(lista1)
+    l2 = len(lista2)
+
+    fusion = []
+
+    i = 0
+    j = 0
+
+    while i < l1 and j < l2:
+        
+        if lista1[i] < lista2[j]:
+            fusion.append(lista1[i])
+            i += 1
+
+        elif lista2[j] < lista1[i]:
+            fusion.append(lista2[j])
+            j += 1
+        
+        elif lista1[i] == lista2[j]:
+            fusion.append(lista1[i])
+            i += 1
+            j += 1
+
+    while i < l1:
+        fusion.append(lista1[i])
+        i += 1
+
+    while j < l2:
+        fusion.append(lista2[j])
+        j += 1
+
+    return fusion
 
 
 #APPROX-SUBSET-SUM (S,t,ε)
@@ -91,4 +124,9 @@ main()
 
 print(TRIM(conjunto, 0.1))
 
+L = [1, 2, 3, 5, 9]
+print([elem + 2 for elem in L])
 
+L1 = [1, 3, 5, 7, 9, 12, 13,22,67]
+L2 = [2, 4, 3,6, 8, 10, 11,14,17,19,78]
+print(MERGE_LISTS(L1,L2))
