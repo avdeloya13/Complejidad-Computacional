501fdb55497dcb19654cdea3b1b0bacc0db78edc
Valeria Deloya <anavaleriadeloya@ciencias.unam.mx>
2024-06-03 21:37:56 -0600

    ejemplar de entrada

--- 3. Meta_heur/N_Reinas.py
+++ 3. Meta_heur/N_Reinas.py
@@ -11,11 +11,33 @@ archivo = sys.argv[1]
 #Abrimos el archivo que contiene al ejemplar
 archivo = open('Ejemplares/' + archivo)
 
+#Almacena la linea 1, que contiene el costo
+costo = archivo.readline()
+
+#Para almacenar el tablero/configuracion inicial
+tablero = []
+
+
+#Leyendo la configuracion inicial
+for linea in archivo:
+	tablero.append(linea.strip('\n')) #va eliminando los \n
+
 
 def main():
 #Funcion main para mandar a llamar todo, ordenado.
 
-	return 1
+	print('EJEMPLAR DE ENTRADA')
+	print('-------------------------')
+
+	print('CONFIGURACION INICIAL: ')
+
+	for i in tablero:
+    	 print(i)
+
+	print('COSTO:') #Nuestro costo van a ser las colisiones
+	print(costo)
+
+	print('SALIDA')
+	print('-------------------------')
 
 main()
-
