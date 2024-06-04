1da849421edd0b74471e3a13380f9838f58d84b9
Valeria Deloya <anavaleriadeloya@ciencias.unam.mx>
2024-06-03 23:14:50 -0600

    inicio columnas

--- 3. Meta_heur/N_Reinas.py
+++ 3. Meta_heur/N_Reinas.py
@@ -17,11 +17,28 @@ costo = archivo.readline()
 #Para almacenar el tablero/configuracion inicial
 tablero = []
 
-
 #Leyendo la configuracion inicial
 for linea in archivo:
 	tablero.append(linea.strip('\n')) #va eliminando los \n
 
+
+def columnas():
+#Funcion que cuenta la cantidad de reinas que se encuentran en cada fila en la configuracion inicial.
+# Debido a que las reinas no pueden ir en una misma fila, a cada reina le corresponde una fila que se le asocia.
+
+	#La configuración del tablero se describe con el vector R.
+	vector_R = []
+
+	for fila in tablero:
+    	 if '1' in fila:
+    		 apariciones = [i for i, x in enumerate(fila) if x == '1']
+
+
+def diagonales():
+#Siendo que a cada reina se le asigna una fila y una columna que no comparte con otra en la funcion anterior, 
+# no existen colisiones entre reinas por filas o columnas; entonces debe analizarse la configuración para las diagonales.
+
+	return 0
 
 def main():
 #Funcion main para mandar a llamar todo, ordenado.
@@ -39,5 +56,7 @@ def main():
 
 	print('SALIDA')
 	print('-------------------------')
+
+	columnas()
 
 main()
